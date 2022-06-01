1ef86cb732447b8c13995f66714577253b47ef41
Peter <peterthomaskim@gmail.com>
2022-06-01 01:10:54 -0300

    jquery fix

--- js/galeria.js
+++ js/galeria.js
@@ -1,18 +1,18 @@
 var dir = "/static";
 var fileextension = ".webp";
 
-$.ajax({
+jQuery.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: dir,
     success: function (data) {
         //List all .png file names in the page
-        $(data)
+        jQuery(data)
             .find("a:contains(" + fileextension + ")")
             .each(function () {
                 var filename = this.href
                     .replace(window.location.host, "")
                     .replace("http://", "");
-                $("#galeria1").append("<img src='"  + filename + "'>");
+                jQuery("#galeria1").append("<img src='"  + filename + "'>");
             })
     },
 });
